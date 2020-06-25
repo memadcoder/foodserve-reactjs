e890d69db97fe7fe6bbf4f7325fdc132aacc3d7a
Madhav Gautam <madhavgautam2016@gmail.com>
2020-06-25 19:50:15 +0545

    components part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import logo from "./logo.svg";
 import "./App.css";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <NavbarBrand href="#">Confusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
