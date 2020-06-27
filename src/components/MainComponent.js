451571c9bf0019ac5425411f69a679abd7e2f919
Madhav Gautam <madhavgautam2016@gmail.com>
2020-06-27 18:29:42 +0545

    React Redux Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
-import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
-import { COMMENTS } from "../shared/comments";
-import { PROMOTIONS } from "../shared/promotions";
-import { LEADERS } from "../shared/leaders";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
